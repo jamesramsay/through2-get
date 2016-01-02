612657a7f920c180bb8aac628b79bd773c287a6c
James Ramsay <git@jwr.vc>
2016-01-02 14:44:52 +1100

    Update: use lodash.get for smaller dependency

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 module.exports = make;
 module.exports.ctor = ctor;
 
-var through2 = require("through2");
-var _ = require("lodash");
-var xtend = require("xtend");
+var through2 = require('through2');
+var get = require('lodash.get');
+var xtend = require('xtend');
 
 var ZERO_BYTE_STRING = '';
 
 function ctor(options, propPath, defaultValue) {
-  if (typeof options !== "object") {
+  if (typeof options !== 'object') {
     defaultValue = propPath;
     propPath = options;
     options = {};
@@ -18,10 +18,10 @@ function ctor(options, propPath, defaultValue) {
   options = xtend({objectMode: true, highWaterMark: 16, excludeZBS: true}, options)
 
   var Get = through2.ctor(options, function (chunk, encoding, cb) {
-    var out = _.get(chunk, propPath, defaultValue);
+    var out = get(chunk, propPath, defaultValue);
 
     // Pushing a zero-byte string in object mode will end the reading process.
-    // In most instances this behviour is undesired.
+    // In most instances this behviour is not desired.
     // https://nodejs.org/api/stream.html#stream_stream_push
     if (options.excludeZBS && out === ZERO_BYTE_STRING) return cb();
 
